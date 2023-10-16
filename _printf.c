78c5dd68b161888cbde412629f63e1a47b230ca1
EnduranceApeh <apehendurance133@gmail.com>
2023-10-16 10:11:51 -0700

    reduced line of code

--- _printf.c
+++ _printf.c
@@ -8,8 +8,7 @@ int _printf(const char *format, ...)
 {
 	va_list list_of_args;
 	int i, length = 0, j;
-	char charac;
-	char *string;
+	char charac, *string;
 	/* check if format is NULL */
 	if (format == NULL)
 		return (-1);
@@ -22,26 +21,29 @@ int _printf(const char *format, ...)
 			_putchar(format[i]);
 			length++;
 		}
+		else
+		{
 		format++;
-		if (format[i] == 'c')
-		{
-			length++;
-			charac = va_arg(list_of_args, int);
-			_putchar(charac);
-		}
-		if (format[i] == 's')
-		{
-			string = va_arg(list_of_args, char*);
-			for (j = 0; string[j] != '\0'; j++)
+			if (format[i] == 'c')
 			{
 				length++;
-				_putchar(string[j]);
+				charac = va_arg(list_of_args, int);
+				_putchar(charac);
 			}
-		}
-		if (format[i] == '%')
-		{
-			length++;
-			_putchar('%');
+			if (format[i] == 's')
+			{
+				string = va_arg(list_of_args, char*);
+				for (j = 0; string[j] != '\0'; j++)
+				{
+					length++;
+					_putchar(string[j]);
+				}
+			}
+			if (format[i] == '%')
+			{
+				length++;
+				_putchar('%');
+			}
 		}
 	}
 	va_end(list_of_args);
