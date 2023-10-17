b843b874e755602b12ce57266084e4b9345b7e59
EnduranceApeh <apehendurance133@gmail.com>
2023-10-17 12:47:50 -0700

    modified code and betty sttyle

--- print_integer.c
+++ print_integer.c
@@ -7,12 +7,13 @@
 
 int print_integer(int value)
 {
-        int i = 0;
-        char buffer[32];
-        int printed = snprintf(buffer, sizeof(buffer), "%d", value);
-        for(i = 0; i < printed; i++)
-        {
-                _putchar(buffer[i]);
-        }
-return (printed);
+	int i = 0;
+	char buffer[32];
+	int printed = snprintf(buffer, sizeof(buffer), "%d", value);
+
+	for (i = 0; i < printed; i++)
+	{
+		_putchar(buffer[i]);
+	}
+	return (printed);
 }
